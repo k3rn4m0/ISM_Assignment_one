10901b64cdc7a5cb8707fe17772852fa780d31cc
vi-hseng <46452844+vi-hseng@users.noreply.github.com>
2020-02-09 23:50:09 -0500

    COMMIT 2
    Updated comments

--- question4.cs
+++ question4.cs
@@ -19,17 +19,18 @@ namespace ISM_Assignment_One
     {
         static void Main(string[] args)
         {
+            // taking input form the user for the number till which the pattern ought to be printed
             Console.Write("n = ");
             int n3 = Convert.ToInt32(Console.ReadLine());
 
 
             Console.WriteLine();
 
-
+            // input for numbers per line
             Console.Write("k = ");
             int k = Convert.ToInt32(Console.ReadLine());
 
-
+            // calling the method 
             UsfNumbers(n3, k);
         }
 
@@ -39,45 +40,45 @@ namespace ISM_Assignment_One
             
             try // Try block
             {
-                for (int i = 1; i <= n3; i++)
+                for (int i = 1; i <= n3; i++) // for loop to print the numbers
                 {
 
-                    if (i % 3 == 0 && i % 5 == 0)
+                    if (i % 3 == 0 && i % 5 == 0) // checking for each number which are multiples of 3 & 5 and replacing them with "US"
                     {
                         Console.Write("US ");
                     }
 
-                    else if (i % 5 == 0 && i % 7 == 0)
+                    else if (i % 5 == 0 && i % 7 == 0) // checking for multiples of 5 & 7 and replacing with SF
                     {
                         Console.Write("SF ");
                     }
 
-                    else if (i % 7 == 0 && i % 3 == 0)
+                    else if (i % 7 == 0 && i % 3 == 0) // checking for multiples of 7 & 3 and replacing it with FU
                     {
                         Console.Write("FU ");
                     }
 
-                    if (i % 3 == 0)
+                    if (i % 3 == 0) // checking for multiples of 3 and replaing with U
                     {
                         Console.Write("U ");
                     }
 
-                    else if (i % 5 == 0)
+                    else if (i % 5 == 0) // checking for multiples of 5 and replacing with S
                     {
                         Console.Write("S ");
                     }
 
-                    else if (i % 7 == 0)
+                    else if (i % 7 == 0) // checking for multiples of 7 and replacing with F
                     {
                         Console.Write("F ");
                     }
 
-                    else
+                    else // if the number is not a multiple, printing the number itself
                     {
                         Console.Write(i + " ");
                     }
 
-                    if (i % k == 0)
+                    if (i % k == 0) // limiting the numbers per line
                     {
                         Console.WriteLine();
                     }
